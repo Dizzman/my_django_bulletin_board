06a4df5d424ee13f2a5f72cb51761191bd7a9c9f
Yako <ysvolodin@gmail.com>
2021-02-04 19:34:45 +0300

    Добавлены функция логина пользователя

--- main/urls.py
+++ main/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
-from .views import BBLoginView, index, other_page
+from .views import BBLoginView, BBLogoutView, index, other_page, profile
 
 
 app_name = 'main'
 urlpatterns = [
+    path('accounts/profile/', profile, name='profile'),
     path('accounts/login/', BBLoginView.as_view(), name='login'),
+    path('accounts/out/', BBLogoutView.as_view(), name='logout'),
     path('<str:page>/', other_page, name='other'),
     path('', index, name='index'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,8 +1,18 @@
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LoginView, LogoutView
 from django.http import HttpResponse, Http404
 from django.template import TemplateDoesNotExist
 from django.template.loader import get_template
 from django.shortcuts import render
+
+
+class BBLoginView(LoginView):
+    template_name = 'main/login.html'
+
+
+class BBLogoutView(LoginRequiredMixin, LogoutView):
+    template_name = 'main/logout.html'
 
 
 def index(request):
@@ -20,5 +30,6 @@ def other_page(request, page):
     return HttpResponse(template.render(request=request))
 
 
-class BBLoginView(LoginView):
-    template_name = 'main/login.html'
+@login_required
+def profile(request):
+    return render(request, 'main/profile.html')
